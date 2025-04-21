6d16179802de86f87624c375235380e6fa62e209
Kelvin <knjuiri@gmail.com>
2025-04-22 00:16:35 +0300

    first commit

---
+++ src/components/Mission.jsx
@@ -0,0 +1,13 @@
+
+
+const Mission = () => {
+    return (
+        <section id="mission">
+            <div className="container mx-auto text-center">
+                <h2 className="mb-8 text-3xl lg:text-4xl">Our Mission</h2>
+            </div>
+        </section>
+    )
+}
+
+export default Mission
